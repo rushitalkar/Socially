52d0f3077333a502bc21ef2fb7c941e6fc798df5
rushitalkar <87230039+rushitalkar@users.noreply.github.com>
2026-08-11 14:20:38 +0530

    feat: enhance database schema with new tables and relations for posts, comments, likes, and follows

--- socially/src/db/schema.js
+++ socially/src/db/schema.js
@@ -1,8 +1,11 @@
 // db/schema.ts
-import { pgTable, serial, text, timestamp ,defaultRandom , uuid } from "drizzle-orm/pg-core";
-
+import { pgTable, serial, text, timestamp ,defaultRandom , uuid  } from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+import { createId } from "@paralleldrive/cuid2";
+import { index, primaryKey } from "drizzle-orm/gel-core";
+import { Table } from "lucide-react";
 export const users = pgTable("users", {
-  id: uuid("id").primaryKey().defaultRandom(),
+  id: text("id").primaryKey().$defaultFn(()=>createId()),
   email: text("email").notNull().unique(),
   username : text("username").notNull().unique(),
   clerkId : text("clerk_id").unique(),
@@ -15,12 +18,83 @@ export const users = pgTable("users", {
   updatedAt: timestamp("updated_at").defaultNow().notNull(),
 });
 
-export const post = pgTable("post",{
-   id : uuid("id").defaultRandom(),
+export const posts = pgTable("post",{
+   id: text("id").primaryKey().$defaultFn(()=>createId()),
    authorId : text("authorId").notNull().references(()=> users.id , {onDelete : "cascade"}),
    content : text("content").notNull(),
    image : text("image"),
    createdAt: timestamp("created_at").defaultNow().notNull(),
    updatedAt: timestamp("updated_at").defaultNow().notNull(),
+   
+})
+
+
+export const postRelations = relations(posts , ({one,many})=>({
+  author : one(users ,{
+    fields : [posts.authorId],
+    references : [users.id]
+  }),
+  comments : many(comments),
+  likes : many(likes),
+  notifications : many(notifications)
+
+}))
+
+export const comments = pgTable(comments ,{
+    id: text("id").primaryKey().$defaultFn(()=>createId()),
+    content : text("content").notNull(),
+    authorId : text("authorId").notNull().references(()=> users.id , {onDelete : "cascade"} ),
+    postId : text("postId").notNull().references(()=> posts.id , {onDelete : "cascade"}),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at").defaultNow().notNull(),
 
 })
+
+export const commentsRelations = relations(comments , ({one,many})=>({
+  author : one(users , {
+    fields : [comments.authorId],
+    references  : [users.id]
+  }),
+  post : one(posts , {
+    fields : [comments.postId],
+    references : [posts.id]
+  }),
+  notifications: many(notifications),
+}))
+
+export const likes = pgTable("likes" , {
+  id: text("id").primaryKey().$defaultFn(()=>createId()),
+  userId : text("userId").notNull().references(()=>users.id ,{onDelete : "cascade"} ),
+  postId : text("postId").notNull().references(()=>posts.id ,{onDelete : "cascade"}),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+
+
+})
+
+export const likesRelations = relations(likes , ({one})=>({
+   user : one(users , {
+    fields : [likes.userId],
+    references : [users.id]
+   }),
+   post : one(posts , {
+    fields : [likes.postId],
+    references : [posts.id]
+   })
+}))
+
+export const follows = pgTable("follows" , {
+  followerId : text("followerId").notNull().references(()=>users.id , {onDelete : "cascade"}),
+  followingId : text("followingId").notNull().references(()=> users.id , {onDelete : "cascade"}),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+}, 
+  (table)=>[
+     primaryKey({columns : [table.followerId , table.followingId]}),
+     index("follows_follower_following_idx").on(
+      table.followerId,
+      table.followingId
+     ),
+    //  we write this way also
+    //  primaryKey({ columns: [table.followerId, table.followingId] }),
+  ]
+)
+
