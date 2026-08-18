fd8789d07477987e07eef0209e345612fec37dbd
rushitalkar <87230039+rushitalkar@users.noreply.github.com>
2026-08-18 14:49:12 +0530

    feat: implement getPosts function to fetch posts with author and comment details; add PostCard component for rendering posts in the UI

--- socially/src/actions/post.action.js
+++ socially/src/actions/post.action.js
@@ -5,6 +5,7 @@ import { auth , currentUser } from "@clerk/nextjs/server";
 import { users , notifications , likes , posts ,comments, follows } from "@/db/schema";
 import { getDbUserId } from "./user.action";
 import { revalidatePath } from "next/cache";
+import { asc, desc } from "drizzle-orm";
 
 export const createPost =async (content , imageUrl)=>{
      try {
@@ -23,4 +24,60 @@ export const createPost =async (content , imageUrl)=>{
         console.error("failed to create post" , error);
         return {success : false , status : 500 , error : error.message}
      }
+}
+
+export const getPosts = async()=>{
+    try {
+      const posts = await db.query.posts.findMany({
+         orderBy : (posts , {desc})=>[desc(posts.createdAt)],
+         // 2. Fetch nested relations using `with`
+         with : {
+         // Fetch post author details
+
+            author : {
+               columns : {
+                  id: true,
+            name: true,
+            image: true,
+            username: true,
+
+               }
+            },
+            // Fetch comments along with comment authors
+            comments : {
+               orderBy : (comments , {asc})=>[asc(comments.createdAt)] ,
+               with : {
+                 author :{
+                  columns : {
+                      id: true,
+                      username: true,
+                      image: true,
+                      name: true,
+                     }
+                  }
+               }
+            },
+            // Fetch array of user IDs who liked the post
+            likes : {
+               columns : {
+                  userId : true
+               }
+            }
+         },
+      })
+      // 3. Map over results to attach comment and like count aggregations
+      const postWithCounts =  posts.map((post)=>({
+         ...post,
+         _count : {
+            likes : post.likes.length,
+            comments : post.comments.length
+         }
+
+      }))
+      return postWithCounts
+
+    } catch (error) {
+       console.log(error);
+        return []; // Return empty array so .map() in UI doesn't crash       
+    }
 }
--- socially/src/app/page.jsx
+++ socially/src/app/page.jsx
@@ -1,13 +1,26 @@
 import { currentUser } from "@clerk/nextjs/server";
 import CreatePost from "@/components/CreatePost";
 import WhoToFollow from "@/components/WhoToFollow";
+import { getPosts } from "@/actions/post.action";
+import PostCard from "@/components/PostCard";
+import { getDbUserId } from "@/actions/user.action";
 export default async function Home() {
   const user = await currentUser();
-
+  const posts = await getPosts();
+  const dbUser = await getDbUserId()
+  
   return (
     <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
         <div className= "lg:col-span-6">
             {user ? <CreatePost/> : null}
+
+            <div className="space-y-4" >
+               {
+                 posts.map((post)=>{
+                  return <PostCard key={post.id} post={post} dbUser={dbUser}/>
+                 })
+               }
+            </div>
         </div>
         <div className="hidden lg:block lg:col-span-4 sticky top-20">
             <WhoToFollow/>
